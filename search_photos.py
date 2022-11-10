9636bd4647075611ec197f66c69f7153b30172b8
Franck Mugisho <fbmugisho@gmail.com>
2022-11-09 20:58:59 -0500

    added logic to return images if any is present and none otherwise.

--- search_photos.py
+++ search_photos.py
@@ -3,8 +3,9 @@ import json
 from opensearchpy import OpenSearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import base64
+import os
 
-key = "road"
+key = "vehicle"
 
 client = boto3.client(
     "lex-runtime",
@@ -20,33 +21,46 @@ service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
+opensearch_client = OpenSearch(
+    hosts = [{'host': host, 'port': 443, 'method': 'GET'}],
+    http_auth = awsauth,
+    use_ssl = True,
+    verify_certs = True,
+    connection_class = RequestsHttpConnection
+)
+
 
 def get_key():
     return key
-
+    
+def clean_dict(dict_obj):
+    """
+    This function removes duplicated values from the dictionary
+    """
+    temp = {val : key for key, val in dict_obj.items()}
+    return {val : key for key, val in temp.items()}
+        
 
 def lambda_handler(event, context):
+    count = 0
+    images_obj = {} # empty object that will store images
     ambiguous_kw = get_key()
-    # need to send message to aws lex
-
     lex_response = client.post_text(
         botName="photobot",
         botAlias="beta",
         userId="378707398066",
         inputText=ambiguous_kw,
     )
+
     
-    # lex_message = lex_response["message"]
-    lex_message = lex_response["slots"]["keywords"]
-    print(lex_message)
-    
-    opensearch_client = OpenSearch(
-        hosts = [{'host': host, 'port': 443, 'method': 'GET'}],
-        http_auth = awsauth,
-        use_ssl = True,
-        verify_certs = True,
-        connection_class = RequestsHttpConnection
-    )
+    try:
+        kw = lex_response["slots"]["keywords"]
+        kw_li = kw.split(" ")
+        # lex_message = " and ".join(kw_li)
+        lex_message=kw
+        
+    except KeyError:
+        lex_message = "" # lex could not disambiguate the query
     
     query = {
         "size": 3000,
@@ -67,12 +81,11 @@ def lambda_handler(event, context):
         bucket_name = hit['_source']['bucket']
         photo_name = hit['_source']['objectKey'] 
         s3client_response = s3client.get_object(Bucket=bucket_name, Key=photo_name)
-        image = s3client_response['Body'].read() 
-        return {
-            'headers': { "Content-Type": "image/png" },
-            'statusCode': 200,
-            'body': base64.b64encode(image).decode('utf-8'),
-            'isBase64Encoded': True
-        }  
+        images_obj[count] = os.path.join("https://", bucket_name + ".s3.amazonaws.com", photo_name)
+        count += 1
     
-     
+    return {
+        'statusCode': 200,
+        'body': clean_dict(images_obj)
+    }
+    
