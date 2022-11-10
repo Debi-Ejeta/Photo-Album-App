1c5d2d80e9242e2789eff6463ec92fd41d6321d7
Franck Mugisho <fbmugisho@gmail.com>
2022-11-09 22:06:51 -0500

    returning event for testing the pipeline. This will be modified

--- search_photos.py
+++ search_photos.py
@@ -22,28 +22,32 @@ credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
 opensearch_client = OpenSearch(
-    hosts = [{'host': host, 'port': 443, 'method': 'GET'}],
-    http_auth = awsauth,
-    use_ssl = True,
-    verify_certs = True,
-    connection_class = RequestsHttpConnection
+    hosts=[{'host': host, 'port': 443, 'method': 'GET'}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
 )
 
 
 def get_key():
     return key
-    
+
+
 def clean_dict(dict_obj):
     """
     This function removes duplicated values from the dictionary
     """
-    temp = {val : key for key, val in dict_obj.items()}
-    return {val : key for key, val in temp.items()}
-        
+    temp = {val: key for key, val in dict_obj.items()}
+    return {val: key for key, val in temp.items()}
+
 
 def lambda_handler(event, context):
     # return json.dumps(event['queryStringParameters'])
     return {
+        'statusCode': 200,
+        'headers': {},
+        'body': {
             "results": [
                 {
                     "url": json.dumps(event['queryStringParameters']),
@@ -52,9 +56,12 @@ def lambda_handler(event, context):
                     ]
                 }
             ]
-            }
+        }
+
+    }
+    
     count = 0
-    images_obj = {} # empty object that will store images
+    images_obj = {}  # empty object that will store images
     ambiguous_kw = get_key()
     lex_response = client.post_text(
         botName="photobot",
@@ -63,16 +70,15 @@ def lambda_handler(event, context):
         inputText=ambiguous_kw,
     )
 
-    
     try:
         kw = lex_response["slots"]["keywords"]
         kw_li = kw.split(" ")
         # lex_message = " and ".join(kw_li)
-        lex_message=kw
-        
+        lex_message = kw
+
     except KeyError:
-        lex_message = "" # lex could not disambiguate the query
-    
+        lex_message = ""  # lex could not disambiguate the query
+
     query = {
         "size": 3000,
         "query": {
@@ -82,21 +88,32 @@ def lambda_handler(event, context):
             }
         }
     }
-    
+
     openSearch_response = opensearch_client.search(
         body=query,
         index='photos'
     )
-    
+
     for hit in openSearch_response['hits']['hits']:
         bucket_name = hit['_source']['bucket']
-        photo_name = hit['_source']['objectKey'] 
+        photo_name = hit['_source']['objectKey']
         s3client_response = s3client.get_object(Bucket=bucket_name, Key=photo_name)
         images_obj[count] = os.path.join("https://", bucket_name + ".s3.amazonaws.com", photo_name)
         count += 1
-    
+
     return {
         'statusCode': 200,
-        'body': clean_dict(images_obj)
+        'headers': {},
+        'body': {
+            "results": [
+                {
+                    "url": json.dumps(event['queryStringParameters']),
+                    "labels": [
+                        "tbd"
+                    ]
+                }
+            ]
+        }
+
     }
-    
+
